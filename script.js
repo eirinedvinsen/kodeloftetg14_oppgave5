dd1ba757ecd1a16cd6d607588032d2b68d7a609a
eirinedvinsen <eirin.edvinsen@gmail.com>
2025-07-14 22:10:14 +0200

    starting on API

--- script.js
+++ script.js
@@ -17,20 +17,25 @@ console.log(foxIcon);
 foxIcon.addEventListener("click", function () {
   const body = document.querySelector("body");
 
-  body.classList.remove("light");
-  body.classList.add("dark");
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+    body.classList.add("light");
+  } else {
+    body.classList.remove("light");
+    body.classList.add("dark");
+  }
 });
 
-async function httpCats (){
+//async function httpCats (){
   const response = await fetch("https://http.cat/[status_code].jpg");
   console.log(response);
 }
 
-httpCats();
+//httpCats();
 
 async function randomFox () {
   const fox = await fetch ("https://randomfox.ca/images/26.jpg");
   console.log(fox);
-}
 
 randomFox()
+
